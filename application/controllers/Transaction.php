3a6942e063e8dfc920917629f08f6d3e5a2e7f55
keiscool2468 <jackli2468@gmail.com>
2017-07-26 10:44:24 +0800

    fk

--- application/controllers/Transaction.php
+++ application/controllers/Transaction.php
@@ -83,7 +83,7 @@ class Transaction extends MY_Controller {
 			$target_object = $this->db->get_where('objects',array('id' => $this->input->post('object_id_2')))->result_array()[0];
 			$target_subCate = $this->db->get_where('sub_categorys',array('id' => $target_object['sub_category_id']))->result_array()[0];
 			$target_Cate = $this->db->get_where('categorys',array('id' => $target_subCate['category_id']))->result_array()[0];
-			$target_user_cate = $this->db->get_where('users_categorys',array('user_id' => $this->mUser->id, 'category_id' => $target_Cate))->result_array()[0];
+			$target_user_cate = $this->db->get_where('users_categorys',array('user_id' => $this->mUser->id, 'category_id' => $target_Cate['id']))->result_array()[0];
 			$target_user_cate['point'] += 1;
 			$this->db->where('id', $target_user_cate['id']);
 			$this->db->update('users_categorys', $target_user_cate);
