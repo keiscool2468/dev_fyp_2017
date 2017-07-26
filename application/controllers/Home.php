4a7bf99b8ac0dc017f4685b0e0d11be27b84dca5
keiscool2468 <jackli2468@gmail.com>
2017-07-26 11:10:25 +0800

    fk

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -54,17 +54,11 @@ class Home extends MY_Controller {
 				}
 				if(sizeof($objects) < 40){
 					$this->db->limit(40-sizeof($objects));
+					$this->db->where_not_in('sub_category_id', $filtedSubCates);
 					$this->db->where('status', 'active');
-					$object2s = array();
-					foreach ($filtedSubCates as $filtedSubCate) {
-						$this->db->where_not_in('sub_category_id', $filtedSubCate);
-						$this->db->order_by("id", "random");
-						$objs = $this->object_model->with('sub_category')->with('location')->with('user')->get_all();
-						foreach ($objs as $obj) {
-							array_push($object2s, $obj);
-						}
-					}
 					// $this->db->or_where_in('expected_location_id', $currUserBehavs[0]['location_id']);
+					$this->db->order_by("id", "random");
+					$object2s = (array) $this->object_model->with('sub_category')->with('location')->with('user')->get_all();
 					$objects = $this->render_order($objects, $currUserCates, $currUserBehavs[0], $object2s);
 				}else{
 					$objects = $this->render_order($objects, $currUserCates, $currUserBehavs[0]);
